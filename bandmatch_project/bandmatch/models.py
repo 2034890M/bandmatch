f1c161b536f49531509334b5d8440872b36a22c1
Jaakko <2160027a@student.gla.ac.uk>
2015-03-09 16:21:34 +0000

    Initial settings done. Working on models

--- bandmatch_project/bandmatch/models.py
+++ bandmatch_project/bandmatch/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+#Player
+
+class Player(models.Model):
+	name = models.CharField(max_length = 128) 	#Forename/surname or just name?
+	email = models.EmailField(max_length=254)
+	contact_info = models.TextField()
+	description = models.TextField()
+	#Preferences can be included in your own description?
+	privacy = models.IntegerField(default=1)
+	demo = models.FileField(upload_to = 'demos', blank = True) #How to have multiple (from 0 to n) demos? 
+	
+
+
+
+
+
+#Message
+
+
+#Band
+
+
+#Advert
--- bandmatch_project/bandmatch_project/settings.py
+++ bandmatch_project/bandmatch_project/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+TEMPLATE_DIRS = [
+#Avoid hard coding the paths, as it is VERY bad practice
+
+TEMPLATE_PATH,
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -82,3 +92,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
