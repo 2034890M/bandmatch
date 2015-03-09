1190b790ff3910c742b17a1997876310427be822
Jaakko <2160027a@student.gla.ac.uk>
2015-03-09 17:24:53 +0000

    Initail modes - done

--- bandmatch_project/bandmatch/models.py
+++ bandmatch_project/bandmatch/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
 
@@ -6,13 +9,18 @@ from django.db import models
 #Player
 
 class Player(models.Model):
-	name = models.CharField(max_length = 128) 	#Forename/surname or just name?
-	email = models.EmailField(max_length=254)
+	user = models.OneToOneField(User)
+
+#	name = models.CharField(max_length = 128) 	#Forename/surname or just name?
+#	email = models.EmailField(max_length = 254)
 	contact_info = models.TextField()
 	description = models.TextField()
 	#Preferences can be included in your own description?
 	privacy = models.IntegerField(default=1)
-	demo = models.FileField(upload_to = 'demos', blank = True) #How to have multiple (from 0 to n) demos? 
+	demo = models.FileField(upload_to = 'player_demos', blank = True) #How to have multiple (from 0 to n) demos? 
+	instrument = models.CharField(max_length = 128) #Need to have this as a list of strings
+	location = models.CharField(max_length = 256)
+	image = models.ImageField(upload_to 'profile_images')
 	
 
 
@@ -20,9 +28,32 @@ class Player(models.Model):
 
 
 #Message
+class Message(models.Model):
+	title = models.CharField(max_length = 128) #May need to be changed
+	content = models.TextField()
+	sender = models.ForeignKey(Player)
+	recipients = models.ManyToManyField(Player)
+	date = models.DateField()
 
 
 #Band
-
+class Band(models.Model):
+	#Genres as a separate field for easier queries?
+	demo = models.FileField(upload_to = 'band_demos', blank = True)
+	location = models.CharField(max_length = 256)
+	description = models.TextField()
+	image = models.ImageField(upload_to = 'band_images')
+	members = models.ManyToManyField(Player)
 
 #Advert
+
+class Advert(models.Model):
+
+#	What need to happen is:
+#	When a band gets deleted - all it's adverts are deleted also!
+
+	band = models.ForeignKey(Band) #Cascade deletion?
+	title = models.CharField(max_length = 128)
+	content = models.TextField()
+	date = models.DateField()
+	looking_for = models.CharField(max_length = 256) #Need to have this as a list??
