3d42cc427ee181d0f824d0e3f6ea01fd81a218b5
Alexmhaley <alexanderhaley@gmail.com>
2015-03-12 18:01:39 +0000

    templates added and urls.py updated

---
+++ bandmatch_project/bandmatch/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import patterns, url
+from bandmatch import views
+
+urlpatterns = patterns('',
+                       
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^your_bands/$', views.your_bands, name='your_bands'), # NEW MAPPING!
+    url(r'^band/(?P<band_name_slug>[\w\-]+)/$', views.band, name='band'),
+    url(r'^profile/(?P<band_name_slug>[\w\-]+)/add_band/$', views.add_band, name='add_band'),
+
+    url(r'^profile/$', views.profile, name='your_profile'),
+    url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
+    url(r'^profile/show/(?P<username>[\w\-]+)', views.profile, name='profile'),
+    url(r'^add_profile/$', views.register_profile, name='register_profile'),
+                       
+    url(r'^search_bands/', views.search_bands, name='search_bands'),
+    url(r'^search_players/', views.search_players, name='search_players'),   
+   
+    )
+#    url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
+#    url(r'^login/$', views.user_login, name='login'),
+    
+#    url(r'^logout/$', views.user_logout, name='logout'),
+# url(r'^search/',views.search,name = 'search'),
+#
+# url(r'^userlist/$', views.userlist, name='userlist'),
+#    url(r'^like_category/$', views.like_category, name='like_category'),
+#    url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
--- bandmatch_project/bandmatch/views.py
+++ bandmatch_project/bandmatch/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+
+    context_dict = {}
+
+    
+
+    response = render(request,'bandmatch/index.html', context_dict)
