a0fcb1700c83939bf88f3b0baccf47b29312b797
Jaakko <2160027a@student.gla.ac.uk>
2015-03-13 09:56:02 +0000

    Fixed syntax errors in urls. Added the remaining views. Still not working?

--- bandmatch_project/bandmatch/views.py
+++ bandmatch_project/bandmatch/views.py
@@ -9,3 +9,40 @@ def index(request):
     
 
     response = render(request,'bandmatch/index.html', context_dict)
+    return response
+
+
+def about(request):
+
+
+	return render(request, 'bandmatch/about.html', {})
+
+
+def your_bands(request):
+
+	return render(request, 'bandmatch/your_bands.html', {})
+
+def band(request):
+	return render(request, 'bandmatch/band.html', {})
+
+
+def add_band(request):
+	return render(request, 'bandmatch/add_band.html', {})
+
+
+def profile(request):
+	return render(request, 'bandmatch/profile.html', {})
+
+def edit_profile(request):
+	return HttpResponse("Template missing")
+
+def register_profile(request):
+	HttpResponse("Template missing")
+
+def search_bands(request):
+	return render(request, 'bandmatch/search_bands.html', {})
+
+def search_players(request):
+	return render(request, 'bandmatch/search_players.html', {})
+
+	
