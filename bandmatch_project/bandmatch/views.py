6e9146e47072495b3ff38ac3de1ac1022d9e9a29
Reni <aqua1111111@gmail.com>
2015-03-14 14:11:23 +0000

    added details in views to be passed to html

--- bandmatch_project/bandmatch/views.py
+++ bandmatch_project/bandmatch/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from bandmatch.models import Band
+from bandmatch.models import Band, Player, Message, Advert
 
 # Create your views here.
 def index(request):
@@ -22,21 +22,81 @@ def about(request):
 
 
 def your_bands(request):
+	context_dict = {}
 
-	return render(request, 'bandmatch/your_bands.html', {})
+	user = request.user
+
+	user_bands = user.band_set.all() #should get all the bands that have that user as a member
+
+	context_dict['bands'] = user_bands
+
+	return render(request, 'bandmatch/your_bands.html', context_dict)
 
 def band(request, band_name_slug):
+	context_dict = {}
 	band = Band.objects.get(slug = band_name_slug)
-	band_name = band.name
-	return render(request, 'bandmatch/band.html', {'name': band_name})
+
+	context_dict['name'] = band.name
+
+	members_list = band.members.all()
+	context_dict['members'] = members_list # a for loop in the html should be able to get the members
+
+	context_dict['location'] = band.location
+
+	context_dict['description'] = band.description
+
+	if band.demo:
+		context_dict['pic'] = band.image.url 
+	else:
+		context_dict['pic']= ''
+
+	if band.demo:
+		context_dict['demo'] = band.demo.url #not sure if this is how you get a file url
+	else:
+		context_dict['demo']= ''
+
+	return render(request, 'bandmatch/band.html', context_dict)
 
 
 def add_band(request):
 	return render(request, 'bandmatch/add_band.html', {})
 
 
-def profile(request):
-	return render(request, 'bandmatch/profile.html', {})
+def profile(request, username):
+	context_dict = {}
+
+	user = User.objects.get(username = username)
+
+	player = Player.objects.get(user = user)
+
+	context_dict['name'] = user.name #should be field for first/sir name
+
+	context_dict['description'] = player.description
+
+	context_dict['instruments'] = player.instruments #should probably be checked how it works when instruments are a list
+
+	#we could do this or pass privacy to the html with the context_dict
+	#and choose what to display there
+	if player.privacy == 1: # if 1 is on and 0 is off
+		context_dict['email'] = user.email #only displayed for registered users and if user allows it
+		context_dict['contact_info'] = player.contact_info 
+		context_dict['location'] = player.location
+	else:
+		context_dict['email'] = ''
+		context_dict['contact_info'] =  ''
+		context_dict['location'] = ''
+
+	if player.demo:
+		context_dict['demo'] = player.demo.url #not sure if this is how you get a file url
+	else:
+		context_dict['demo']= ''
+
+	if player.image:
+		context_dict['pic'] = player.image.url
+	else:
+		context_dict['pic'] = ''
+
+	return render(request, 'bandmatch/profile.html', context_dict)
 
 def edit_profile(request):
 	return HttpResponse("Template missing")
@@ -45,6 +105,7 @@ def register_profile(request):
 	HttpResponse("Template missing")
 
 def search_bands(request):
+	#should only search in bands, might be able to do it without bing, maybe not
 	return render(request, 'bandmatch/search_bands.html', {})
 
 def search_players(request):
